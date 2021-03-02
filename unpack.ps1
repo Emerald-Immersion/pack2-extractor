9e268daf50fbd6d3b2b44412ba1d0b23b6758663
brakenium <brakenium@example.com>
2021-03-02 17:14:18 +0100

    Add FSB extraction

--- unpack.ps1
+++ unpack.ps1
@@ -1,9 +1,17 @@
-mkdir output
-
-gci "input\*.pack2" | % {
+Get-ChildItem "input\*.pack2" | ForEach-Object {
     $dest = "output\$($_.BaseName)"
+
 	if (-not [IO.Directory]::Exists($dest)) {
         mkdir $dest
-        & .\offzip\offzip.exe -z 15 -a "$($_.FullName)" "$dest" 2>&1 | out-null
+        mkdir "$($dest)\fsb_extract" | out-null
+        & .\bin\offzip\offzip.exe -z 15 -a "$($_.FullName)" "$dest" 2>&1 | out-null
     }
+}
+
+Get-ChildItem "output\*\*.fsb" | ForEach-Object {
+    $dest = "$($_.Directory)\fsb_extract"
+    if (-not [IO.Directory]::Exists($dest)) {
+        mkdir $dest 2>&1 | out-null
+    }
+    & .\bin\fsbext\fsbext.exe -d $dest $_.FullName 2>&1 | out-null
 }
